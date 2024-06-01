3fda383527b1f23e1713dc39e904f0f3b486868a
Ömer Arpacı <arpaci.omer2@gmail.com>
2024-06-02 02:35:50 +0300

    Entities created and packages are installed into layers

--- EntityLayer/Concrete/Destination.cs
+++ EntityLayer/Concrete/Destination.cs
@@ -13,5 +13,8 @@ namespace EntityLayer.Concrete
         public string DayNight { get; set; }
         public double Price { get; set; }
         public string Image { get; set; }
+        public string Description { get; set; }
+        public int Capacity { get; set; }
+        public bool Status { get; set; }
     }
 }
