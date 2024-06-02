0fc95477f156e1a89e5b7661b68d77a644fa2a9d
Ömer Arpacı <arpaci.omer2@gmail.com>
2024-06-02 18:24:24 +0300

    Fetching Entity data from sql added

---
+++ TraversalCoreProject/ViewComponents/Default/_PopularDestinations.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _PopularDestinations:ViewComponent
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}
