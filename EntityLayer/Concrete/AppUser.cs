f1af0191e3b30f789d060b653fd79839624158d6
Ömer Arpacı <arpaci.omer2@gmail.com>
2024-06-05 03:06:33 +0300

    Identity tables have migrated.

--- EntityLayer/Concrete/AppUser.cs
+++ EntityLayer/Concrete/AppUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace EntityLayer.Concrete
 {
-    public class AppUser: Identity
+    public class AppUser: IdentityUser<int>
     {
+        public string ImageUrl { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Gender { get; set; }
     }
 }
