d006db890175e6c8070561473729cd33ada026ad
Ömer Arpacı <arpaci.omer2@gmail.com>
2024-06-06 14:55:18 +0300

    Login and Register UI added.

---
+++ TraversalCoreProject/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Controllers
+{
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+      
+        }
+
+        //[HttpPost]  
+        //IActionResult SignUp()
+        //{
+        //    return View();
+        //}
+    
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //IActionResult SignIn()
+        //{
+        //    return View();
+        //}
+    }
+}
