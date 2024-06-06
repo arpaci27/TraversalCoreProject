f58bd4fa02a5c033e375d6c5bb7428694f4c1f92
Ömer Arpacı <arpaci.omer2@gmail.com>
2024-06-06 15:20:49 +0300

    UserRegisterViewModel clas has added.

--- TraversalCoreProject/Controllers/LoginController.cs
+++ TraversalCoreProject/Controllers/LoginController.cs
@@ -1,25 +1,36 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TraversalCoreProject.Models;
 
 namespace TraversalCoreProject.Controllers
 {
     [AllowAnonymous]
     public class LoginController : Controller
     {
-        [HttpGet]
-        public IActionResult SignUp()
-        {
-            return View();
-      
-        }
+        private readonly UserManager<AppUser> _userManager;
 
-        //[HttpPost]  
-        //IActionResult SignUp()
-        //{
-        //    return View();
-        //}
-    
-        [HttpGet]
+		public LoginController(UserManager<AppUser> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		[HttpGet]
+		public IActionResult SignUp()
+		{
+			return View();
+
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> SignUp(UserRegisterViewModel p)
+		{
+			return View();
+		}
+
+		[HttpGet]
         public IActionResult SignIn()
         {
             return View();
