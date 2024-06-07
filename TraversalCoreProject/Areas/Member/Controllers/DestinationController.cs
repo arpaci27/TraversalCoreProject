ad41fb436083b516df36860e2b4fccdc18e0f785
Ömer Arpacı <arpaci.omer2@gmail.com>
2024-06-08 02:33:25 +0300

    Listing Destination data in tables from DB

---
+++ TraversalCoreProject/Areas/Member/Controllers/DestinationController.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [AllowAnonymous]
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}
