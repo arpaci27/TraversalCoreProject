d40a3d595dacee136f48ce6c59e265b87f30c246
Ömer Arpacı <arpaci.omer2@gmail.com>
2024-06-15 01:44:11 +0300

    Admin area created and Admin Panel UI added.

--- TraversalCoreProject/Startup.cs
+++ TraversalCoreProject/Startup.cs
@@ -78,6 +78,14 @@ namespace TraversalCoreProject
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+
         }
     }
 }
---
+++ TraversalCoreProject/ViewComponents/MemberDashboard/_PlatformSetting.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.MemberDashboard
+{
+    public class _PlatformSetting:ViewComponent
+    {
+
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+
+    }
+}
