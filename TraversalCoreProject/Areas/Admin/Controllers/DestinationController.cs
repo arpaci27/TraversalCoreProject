a1206ad328eabf69bb67fc61d708d8aa61c71cd8
Ömer Arpacı <arpaci.omer2@gmail.com>
2024-06-15 03:40:01 +0300

    Create new  destination adding, Deleting and Updating existing destination amd Listing destinations as an admin. (Destination CRUD)

--- TraversalCoreProject/Areas/Admin/Controllers/DestinationController.cs
+++ TraversalCoreProject/Areas/Admin/Controllers/DestinationController.cs
@@ -1,13 +1,50 @@
-﻿    using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProject.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class DestinationController : Controller
     {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
         public IActionResult Index()
         {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddDestination()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddDestination(Destination destination)
+        {
+            destinationManager.TAdd(destination);
+            return RedirectToAction("Index", "Destination", new { area = "Admin" });
+        }
+
+        public IActionResult DeleteDestination(int id)
+        {
+            var values = destinationManager.TGetByID(id);
+            destinationManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateDestination(int id)
+        {
+            var values = destinationManager.TGetByID(id);   
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdateDestination(Destination destination) { 
+            destinationManager.TUpdate(destination);
+            return RedirectToAction("Index");
         }
     }
 }
